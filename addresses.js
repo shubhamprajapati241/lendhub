7049461500b95da550ba69e515400344f7f69001
Shubham Prajapati <61042463+shubhamprajapati241@users.noreply.github.com>
2023-03-06 07:09:19 +0530

    Fix : Address.js

--- addresses.js
+++ addresses.js
@@ -1,6 +1,13 @@
-// export const BankContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// Deployed on Goerli Test Net
 
-export const BankContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // goerli
+// npx hardhat run scripts/deploy.js --network goerli
+export const BankContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const AddressStorage = "0xc52DD5479516c332e45Ede69cA198d1773585473";
+export const AddressToTokenMap = "0x1630654d352CF246E89d4B5475781Df25a98b1C4";
+export const LendingConfig = "0x62194ef7094267A0dD7a639Dad67A5221EfA65D8";
+export const LendingPoolV2 = "0xC6fcbAD86BE5e06511F59D1eaD5476e0210782d6";
+export const LendingPoolAddressProvider =
+  "0x2F1bd3AE5247C8FF898e2995F505aFFCf4F022f6";
 
 // npx hardhat run scripts/deploy.js --network localhost
 
@@ -10,10 +17,13 @@ export const BankContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 // Address of LendingPoolV2 :  0x0165878A594ca255338adfa4d48449f69242Eb8F
 // Address of LendingPoolAddressProvider :  0xa513E6E4b8f2a923D98304ec87F64353C4D5C853
 
-// npx hardhat run scripts/deploy.js --network goerli
+// Chainlink pricefeed
+// ETH / USD  => 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+// DAI / USD  => 0x0d79df66BE487753B02D015Fb622DED7f0E9798d
+// USDC / USD => 0xAb5c49580294Aff77670F839ea425f5b78ab3Ae7
+// LINK / USD => 0x48731cF7e84dc94C5f84577882c14Be11a5B7456
 
-// Address of AddressStorage :  0xc52DD5479516c332e45Ede69cA198d1773585473
-// Address of AddressToTokenMap :  0x1630654d352CF246E89d4B5475781Df25a98b1C4
-// Address of LendingConfig :  0x62194ef7094267A0dD7a639Dad67A5221EfA65D8
-// Address of LendingPoolV2 :  0xC6fcbAD86BE5e06511F59D1eaD5476e0210782d6
-// Address of LendingPoolAddressProvider :  0x2F1bd3AE5247C8FF898e2995F505aFFCf4F022f6
+// Metamask Wallet Token Addresses
+// DAI  - 0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60 - confirmed Uniswap and wallet
+// LINK - 0x326C977E6efc84E512bB9C30f76E30c160eD06FB - confirmed Uniswap and wallet
+// USDC - 0x07865c6E87B9F70255377e024ace6630C1Eaa37F
